fb39fc7d97d8c5efaa1df4a5e8a6f296c0de370c
Oliwer Galaszkiewicz <156899800+O-Galaszkiewicz@users.noreply.github.com>
2024-12-11 15:35:10 +0000

    Update index.mjs

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -3,6 +3,7 @@ const loginDiv = document.getElementById("loginDiv");
 const registrationDiv = document.getElementById("registrationDiv");
 const homeDiv = document.getElementById("homeDiv");
 const makePostDiv = document.getElementById("makePostDiv");
+const postsContainer = document.getElementById("postsList");
 
 const loginButton = document.getElementById("loginButton");
 const switchToRegister = document.getElementById("switchToRegister");
